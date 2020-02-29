2b9d5401faf2fbdcc13ae604475603320a49e357
John O <>
2020-02-29 13:49:25 +0200

    Added classes AbstractDAO, Pet_Vaccine and Vaccine...

---
+++ src/main/java/ee/sda/AbstractDAO.java
@@ -0,0 +1,84 @@
+package ee.sda;
+
+import ee.sda.DB.DatabaseUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public abstract class AbstractDAO <T, K> {
+
+    private Class<T> entityClazz;
+
+    public AbstractDAO(Class<T> entityClazz) {
+        this.entityClazz = entityClazz;
+    }
+
+    public T save(T object) {
+        Transaction transaction = null;
+
+        try {
+            Session session = DatabaseUtil.getSessionFactory().openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+
+            K id = (K) session.save(object);
+
+            transaction.commit();
+
+            return find(id);
+        } catch (HibernateException e) {
+            transaction.rollback();
+            return null;
+        }
+
+    }
+
+    public void update(T object) {
+        Transaction transaction = null;
+
+        try {
+            Session session = DatabaseUtil.getSessionFactory().openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+
+            session.update(object);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
+
+    }
+
+    public void delete(T object) {
+        Transaction transaction = null;
+
+        try {
+            Session session = DatabaseUtil.getSessionFactory().openSession();
+            transaction = session.getTransaction();
+
+            transaction.begin();
+
+            session.delete(object);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
+    }
+
+    public T find(K id) {
+        Session session = DatabaseUtil.getSessionFactory().openSession();
+        T object = session.find(entityClazz, id);
+        return object;
+    }
+
+    // public List<SpecificCLASS> findAll()
+    public List<T> findAll() {
+        Session session = DatabaseUtil.getSessionFactory().openSession();
+        return session.createCriteria(entityClazz).list();
+        // return session.createCriteria(Job.class).list()
+    }
+}
