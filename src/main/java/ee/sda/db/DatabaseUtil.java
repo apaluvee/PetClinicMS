586fcce98dab295b4677d17319ee6aed1817be81
Alar Paluvee <alar260@hotmail.com>
2020-03-01 09:50:33 +0200

    changed database

--- src/main/java/ee/sda/db/DatabaseUtil.java
+++ src/main/java/ee/sda/db/DatabaseUtil.java
@@ -22,9 +22,9 @@ public class DatabaseUtil {
             Configuration configuration = new Configuration();
             Properties properties = new Properties();
             properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/petclinicMS");
-            properties.put(Environment.USER, "root");
-            properties.put(Environment.PASS, "13ddonjee");
+            properties.put(Environment.URL, "jdbc:mysql://saeed.chcsf7pbopuv.eu-west-2.rds.amazonaws.com/petclinicMS");
+            properties.put(Environment.USER, "sekbodb");
+            properties.put(Environment.PASS, "sekbo2018");
             properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
             properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
             properties.put(Environment.HBM2DDL_AUTO, "none");
