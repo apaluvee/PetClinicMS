37def351d728a2f1b6dbe2d0b8e5614c8f35f28c
Priit Herja <priit218@gmail.com>
2020-03-01 14:13:32 +0200

    consultation fixed

--- src/main/java/ee/sda/dao/ConsultationDAO.java
+++ src/main/java/ee/sda/dao/ConsultationDAO.java
@@ -2,15 +2,22 @@ package ee.sda.dao;
 
 import ee.sda.entities.Consultation;
 import ee.sda.entities.Pet;
+import ee.sda.entities.Veterinarian;
 
 import java.time.LocalDate;
 
-public class ConsultationDAO extends AbstractDAO<Consultation, Integer > {
-
+public class ConsultationDAO extends AbstractDAO<Consultation, String > {
     public ConsultationDAO() {
         super(Consultation.class);
     }
 
+    public Consultation createConsultation(LocalDate consultDate, String consultDescription, Integer petId, Integer VetId){
+        PetDAO petDao= new PetDAO();
+        Pet pet=petDao.find(petId);
+        VeterinarianDAO veterinarianDAO=new VeterinarianDAO();
+        Veterinarian veterinarian=veterinarianDAO.find(VetId);
+        return save(new Consultation(consultDate,consultDescription,pet,veterinarian));
+    }
 
 
 }
