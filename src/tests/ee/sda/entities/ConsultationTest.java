2bcc15cd70b04c535710bd91a44e2eddd0fe19b5
John <email@email.com>
2020-03-04 21:48:20 +0200

    modified pom file

---
+++ src/tests/ee/sda/entities/ConsultationTest.java
@@ -0,0 +1,30 @@
+package ee.sda.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultationTest {
+
+    Consultation consultation = new Consultation();
+    @Test
+    void getId() {
+        consultation.getId()
+    }
+
+    @Test
+    void getConsultDate() {
+    }
+
+    @Test
+    void getConsultDescription() {
+    }
+
+    @Test
+    void getPet() {
+    }
+
+    @Test
+    void getVeterinarian() {
+    }
+}
