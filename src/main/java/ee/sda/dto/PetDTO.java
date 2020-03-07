6308177af652bd183150a25e26b402db12daec28
Jjiro <email@email.com>
2020-03-07 14:01:06 +0200

    Service layer created...

--- src/main/java/ee/sda/dto/PetDTO.java
+++ src/main/java/ee/sda/dto/PetDTO.java
@@ -5,20 +5,34 @@ import ee.sda.entities.Pet;
 
 import java.time.LocalDate;
 
-public class PetDTO {
+public class PetDTO extends BaseDTO {
 
+    int id;
     String petName;
     String ownerName;
     String species;
     LocalDate dob;
 
+    public PetDTO() {
+    }
 
-   public PetDTO(Pet pet) {
-       this.petName = pet.getPetName();
-       this.ownerName = pet.getOwnerName();
-       this.species = pet.getSpecies();
-       this.dob = pet.getDob();
-   }
+    public PetDTO(int id) {
+        this.id = id;
+    }
+
+    public PetDTO(int id, String petName) {
+        this.id = id;
+        this.petName = petName;
+    }
+
+    public PetDTO(Pet pet) {
+        this.id = pet.getId();
+        this.petName = pet.getPetName();
+        this.ownerName = pet.getOwnerName();
+        this.species = pet.getSpecies();
+        this.dob = pet.getDob();
+    }
+
 
     public String getJson() {
         Gson gson = new Gson();
